2fe6df4c67b01c30ade50744c2ffa59fa1f73ecf
Nicolas <nicolasroth001@gmail.com>
2025-11-01 16:42:04 -0500

    Initial commit: FixMate Pro Starter v1.1

--- fixmate.ps1
+++ fixmate.ps1
@@ -48,4 +48,17 @@ if ($Json) {
 }
 
 Write-Host "`nLogged to: $logFile"
+Write-Host "`nDiagnosis:"
+Write-Host "The error message indicates that the user `"DevUser`" does not have the necessary permissions to describe instances in Amazon EC2, as there is no identity-based policy allowing the `"ec2:DescribeInstances`" action for this user.`n"
+
+Write-Host "Fix Steps:"
+Write-Host "1. Log in to the AWS Management Console using an account with administrative permissions."
+Write-Host "2. Navigate to the IAM service."
+Write-Host "3. Locate the user `"DevUser`" under the Users section."
+Write-Host "4. Click on the user to view their details."
+Write-Host "5. Click on the `"Add permissions`" button."
+Write-Host "6. Choose `"Attach policies directly`"."
+Write-Host "7. In the search box, enter “AmazonEC2ReadOnlyAccess” to find the managed policy."
+Write-Host "8. Select the policy and click `"Attach policy`"."
+Write-Host "9. Ask the user to log out and log back in to apply the new permissions.`n"
 
